940f31ad62c81715624a3c3eee3324c792d9218a
Guruprasad Kulkarni <comdotlinux@users.noreply.github.com>
2020-11-21 20:04:03 +0000

    making shell check happy

--- lib/utils.bash
+++ lib/utils.bash
@@ -60,10 +60,10 @@ install_version() {
   # Added architecture detection
   local architecture=""
   case $(uname -m) in
-      i386)   architecture="386" ;;
-      i686)   architecture="386" ;;
-      x86_64) architecture="amd64" ;;
-      arm)    dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
+    i386)   architecture="386" ;;
+    i686)   architecture="386" ;;
+    x86_64) architecture="amd64" ;;
+    arm)    dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
   esac
 
   if [ "$install_type" != "version" ]; then
