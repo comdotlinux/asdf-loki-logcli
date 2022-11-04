86552a1391d73285b7ff02e3e140f367cdab6341
Guruprasad Kulkarni <comdotlinux@users.noreply.github.com>
2022-11-04 09:23:37 +0000

    Excluded helm-loki tags

--- lib/utils.bash
+++ lib/utils.bash
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash -x
+#!/usr/bin/env bash
 
 set -euo pipefail
 
@@ -24,7 +24,7 @@ sort_versions() {
 
 list_github_tags() {
   git ls-remote --tags --refs "$GH_REPO" |
-    grep -o 'refs/tags/.*' | cut -d/ -f3- |
+    grep -o 'refs/tags/.*' | grep -v "helm-loki" | cut -d/ -f3- |
     sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
